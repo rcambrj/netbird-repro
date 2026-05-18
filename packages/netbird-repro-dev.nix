eea34450087415bd2dc70e0db77602d609e8ba88
Robert Cambridge <robert@cambridge.me>
2026-05-18 23:29:29 +0200

    patch netbird. works but need to audit the diff

--- packages/netbird-repro-dev.nix
+++ packages/netbird-repro-dev.nix
@@ -1,19 +1,24 @@
 { flake, pkgs, ... }:
 
 let
-  netbirdManagementPatches = [
-    # Add local netbird-management patches here while iterating on netbird-repro-dev.
+  netbirdClientPatches = [
+    ../patches/netbird-client-01-firewall-interface.patch
+    ../patches/netbird-client-02-nftables-acl.patch
+    ../patches/netbird-client-03-nftables-manager.patch
+    ../patches/netbird-client-04-iptables-manager.patch
+    ../patches/netbird-client-05-uspfilter-manager.patch
+    ../patches/netbird-client-06-acl-manager.patch
   ];
 in
 pkgs.callPackage ./netbird-repro {
   netbirdPackage = pkgs:
     pkgs.callPackage ./netbird {
       inherit flake;
+      patches = netbirdClientPatches;
     };
 
   netbirdManagementPackage = pkgs:
     pkgs.callPackage ./netbird-management {
       inherit flake;
-      patches = netbirdManagementPatches;
     };
 }
